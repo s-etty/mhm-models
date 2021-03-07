54ca6bc0d645e203483da8907b9b2f8a39f527a2
s-etty <5265145+s-etty@users.noreply.github.com>
2021-03-06 19:55:38 -0800

    create parametric cox hazard model

--- create-models.R
+++ create-models.R
@@ -1,7 +1,8 @@
 library(survival)
+library(survminer)
 library(dplyr)
 
-main_lot <- lots %>%
+main_lot_closed <- lots %>%
   filter(lot_name == "Main") %>%
   group_by(date) %>%
   filter(status == "Standby" | status == "Closed") %>%
@@ -9,14 +10,33 @@ main_lot <- lots %>%
   mutate(time_to_close = difftime(adjusted_timestamp,
                                   ymd_hm("2021-01-01 07:00", tz = "America/Los_Angeles"),
                                   units = "mins"),
-         status = TRUE)
+         status = TRUE,
+         weekday = as.factor(weekday))
+
+main_lot_open <- lots %>%
+  filter(lot_name == "Main") %>%
+  group_by(date) %>%
+  filter(n_distinct(status) == 1) %>%
+  arrange(desc(collection_timestamp)) %>%
+  slice(1) %>%
+  mutate(time_to_close = difftime(adjusted_timestamp,
+                                  ymd_hm("2021-01-01 07:00", tz = "America/Los_Angeles"),
+                                  units = "mins"),
+         status = FALSE)
+
+main_lot <- bind_rows(main_lot_closed, main_lot_open) %>%
+  mutate(weekday = as.factor(weekday)) %>%
+  arrange(collection_timestamp)
 
 saturdays <- main_lot %>%
   filter(weekday == "Sat")
 
 f1 <- survfit(Surv(time_to_close, status) ~ 1, data = main_lot)
+cox_model <- coxph(Surv(time_to_close, status) ~ weekday, data = main_lot_closed)
 
 saturdays_f1 <- survfit(Surv(time_to_close, status) ~ 1, data = saturdays)
 
-plot(f1)
-plot(saturdays_f1)
+ggsurvplot(f1)
+ggsurvplot(saturdays_f1)
+ggforest(cox_model)
+cox_model
